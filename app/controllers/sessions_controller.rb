559a60b2166627eec6db6c16699adb278180f9c9
Ethan <ethanolivieri56@gmail.com>
2025-04-07 09:32:13 -0400

    Added functionality and improved styling to login screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Logged in successfully"
+      redirect_to chatroom_path
+    else
+      flash[:alert] = "Incorrect username or password entered. Please try again."
+      render "new"
+    end
+  end
 end
