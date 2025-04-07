900bfb874640c4e5fb1b3669895d150dd76ae49c
Ethan <ethanolivieri56@gmail.com>
2025-04-07 10:28:59 -0400

    Added Success and Error Messages with styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
+      flash[:success] = "Logged in successfully"
       redirect_to root_path
     else
-      flash.now[:alert] = "Incorrect username or password entered. Please try again."
+      flash.now[:error] = "Incorrect username or password entered. Please try again."
       render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out successfully"
+    flash[:success] = "Logged out successfully"
     redirect_to login_path, status: :see_other
   end
 end
